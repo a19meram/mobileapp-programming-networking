5ecc01d999f876b10d1c2da3ac8c6912165f9275
a19meram <a19meram@student.his.se>
2021-04-15 10:39:58 +0200

    Because of maountain class i changed url to params!!

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,20 +36,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+
+
 
         mountainArrayList = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this,R.layout.list_item_textview, R.id.my_listview, mountainArrayList);
+        adapter = new ArrayAdapter<>(this,R.layout.list_item_textview, R.id.list_item_textview, mountainArrayList);
 
         ListView my_listview = (ListView) findViewById(R.id.my_listview);
         my_listview.setAdapter(adapter);
-
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
         my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String name = mountainArrayList.get(position).getName("name");
                 String location = mountainArrayList.get(position).getLocation("Location");
                 int size = mountainArrayList.get(position).getSize("size");
+
+                String meddelande = name + " is located in mountain range " + location + " and reaches " + size + " m above the sea level.";
+
+                Toast.makeText(MainActivity.this, meddelande, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -67,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         protected String doInBackground(String... params) {
             try {
-                URL url = new URL("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+                URL url = new URL(params[0]);
                 connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
 
@@ -110,8 +116,10 @@ public class MainActivity extends AppCompatActivity {
                     String location = jsonObject.getString("location");
                     int size = jsonObject.getInt("size");
 
+
                     mountainArrayList.add(new Mountain(name, size, location));
                 }
+
                 /*
                 final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item_textview, R.id.list_item_textview, mountainArrayList);
                 final ListView my_listview = (ListView) findViewById(R.id.my_listview);
